9990ab686c8929403dcb5bd3eedf43a005aa5e6a
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-09-08 18:13:08 +0530

    react

--- src/components/greet.js
+++ src/components/greet.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-function Greet(){
-    return <h1>Hello Vaidehi</h1>
-}
+// function Greet(){
+//     return <h1>Hello Vaidehi</h1>
+// }
+
+const Greet = () => {};
 
 export default Greet;
