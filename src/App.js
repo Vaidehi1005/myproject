303ae77eb97572f858a1af6ef29800f95dcd23d2
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-09-08 18:44:42 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Greet } from "./components/greet";
+// import my from "./components/greet";
+
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <my />
+
     </div>
   );
 }
