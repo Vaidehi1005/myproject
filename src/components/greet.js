af40630fe4e81254989420298a3712586a3b2670
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-09-12 08:46:01 +0530

    react

--- src/components/greet.js
+++ src/components/greet.js
@@ -6,11 +6,11 @@ import React from "react";
 
 //  export const Greet = () => <h1>Hello Vaidehi</h1>;
 const Greet = (props) => {
-  console.log(props)
-  return <h1>Hello Vaidehi</h1>
-}
+  console.log(props);
+  return <h1>Hello {props.name}</h1>;
+};
 
 export default Greet;
 
 // functional components use optionally a props and it is normally js function that returns the jsx or html
-// components no code reuse kari sakie and props ek optional property che je component accept kare input and e component ne allow kare to be dynamic aapde app component ma ek property add karisu e name che je app component to greet component pass thase and then browser ma render thase and e props ne aapde components ma use karvu che to as a attribute use karvanu and ema e attribute ma value pn aqpvvani and attribute je che e aapne e componenet ni property aapse and if e value greet component ma joie to pela aapde parameter add karvanu props functional component ma name koi pn aapi sakie pn by default ej che and aapne curly braces lagava padse then aapde console log karisu props ne and return karisu h1 tag ne
+// components no code reuse kari sakie and props ek optional property che je component accept kare input and e component ne allow kare to be dynamic aapde app component ma ek property add karisu e name che je app component to greet component pass thase and then browser ma render thase and e props ne aapde components ma use karvu che to as a attribute use karvanu and ema e attribute ma value pn aqpvvani and attribute je che e aapne e componenet ni property aapse and if e value greet component ma joie to pela aapde parameter add karvanu props functional component ma name koi pn aapi sakie pn by default ej che and aapne curly braces lagava padse then aapde console log karisu props ne and return karisu h1 tag ne.props ek object che jema attributes and value che e aapde parent componenet ma pass kari sake
