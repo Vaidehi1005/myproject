6d5581873a645441a7103e915de9d3053e0d8550
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-10-08 22:57:07 +0530

    react

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Hello = () => {
   //   return(
-  //       <div className:"bunny"> 
+  //       <div className:"bunny">
   //           <h1>Hello Vaidehi!</h1>
   //       </div>
   //   )
   // }
-  // aa upar valu je che e jsx use kaine lakyhu che 
+  // aa upar valu je che e jsx use kaine lakyhu che
   // aapde ek simple functional component banaie che and aapde ema div return karie che jema h1 tag che
 
   return React.createElement(
@@ -16,5 +16,5 @@ const Hello = () => {
     React.createElement("h1", null, "Hello Vaidehi")
   );
 };
-// aa method without jsx che and aama 3 return React.createElement() karvanu ema element banavanu pela 1st parameter element aavse and 2nd parameter additional properties 
-export default Hello
+// aa method without jsx che and aama 3 return React.createElement() karvanu ema element banavanu pela 1st parameter element aavse and 2nd parameter additional properties and 3 property ma element aavse if navu element banavu hase to then ema farithi reactcreateelement karvanu 
+export default Hello;
