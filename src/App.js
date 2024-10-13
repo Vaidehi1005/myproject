d1435b3a95d2ace897a1ab7e0022f09beca69e02
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-10-14 00:09:58 +0530

    react

--- src/App.js
+++ src/App.js
@@ -7,33 +7,33 @@ import Welcome from "./components/Welcome";
 import Hello from "./components/hello";
 import { Component } from "react";
 
-class App extends Component{
-  render () {
-    return(
-      <div className="App">
-        {/* <Greet /> */}
-        {/* <Welcome /> */}
-        {/* <Hello /> */}
-      </div>
-    )
-  }
-}
-// function App() {
-//   return (
-//     <div className="App">
-//       <Greet name="Vaidehi" heroName="Spiderman">
-//         <p>This is a children props</p>
-//       </Greet>
-//       <Greet name="Parthvi" heroName="Batman">
-//         <button>Applied</button>
-//       </Greet>
-//       <Greet name="Shivansh" heroName="Ironman" />
-//       <Welcome name="Vaidehi" heroName="Spiderman" />
-//       <Welcome name="Parthvi" heroName="Batman" />
-//       <Welcome name="Shivansh" heroName="Ironman" />
-//       {/* <Hello /> */}
-//     </div>
-//   );
+// class App extends Component{
+//   render () {
+//     return(
+//       <div className="App">
+//         {/* <Greet /> */}
+//         {/* <Welcome /> */}
+//         {/* <Hello /> */}
+//       </div>
+//     )
+//   }
 // }
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Vaidehi" heroName="Spiderman">
+        <p>This is a children props</p>
+      </Greet>
+      <Greet name="Parthvi" heroName="Batman">
+        <button>Applied</button>
+      </Greet>
+      <Greet name="Shivansh" heroName="Ironman" />
+      <Welcome name="Vaidehi" heroName="Spiderman" />
+      <Welcome name="Parthvi" heroName="Batman" />
+      <Welcome name="Shivansh" heroName="Ironman" />
+      {/* <Hello /> */}
+    </div>
+  );
+}
 
 export default App;
