4d8ac7c02c81a19c0879185018545f5486055fe0
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-10-13 23:17:27 +0530

    react

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component{
       <div className="App">
         {/* <Greet /> */}
         {/* <Welcome /> */}
-        <Hello />
+        {/* <Hello /> */}
       </div>
     )
   }
