53909f77115acb0008df425ffc86a1eeb6314fce
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2025-03-03 19:50:18 +0530

    code

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //aano matlab che k je index.html ma root element che ema react no jetlo pn html no code hase e badho ema lakhase and aa niche je lakhyu che e jsx che je html jevu dekhai che but e nathi
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
