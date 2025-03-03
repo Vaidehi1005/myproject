06c1d5fd217b3908df02d57d6a247eaf554aa43d
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2025-03-03 23:25:19 +0530

    code

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,6 +1,28 @@
 const App = () => {
-  return <div>My first component</div>;
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img
+            src="https://images.seeklogo.com/logo-png/9/1/nike-logo-png_seeklogo-99478.png"
+            alt="brand"
+          />
+        </div>
+
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>
+            Login
+        </button>
+      </nav>
+    </div>
+  );
 };
 //App component che and component resuable che e koi ma pn use kari sake
-
+//react ma aapde class ne as classname use karie che as aapde class html ma use karie che so
 export default App;
