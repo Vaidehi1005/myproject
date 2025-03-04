d3ef31c21c3c6845ca3c8aaf7d6ae4c6cacac7b1
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2025-03-04 11:10:59 +0530

    code

--- hello-world/src/components/Hero.jsx
+++ hello-world/src/components/Hero.jsx
@@ -3,14 +3,25 @@ const Hero = () => {
         <main className="Hero">
             <div className="Hero-content">
             <h1>YOUR FEET DERSERVE THE BEST</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit temporibus dolore porro quasi magni incidunt cupiditate, fugit vero numquam</p>
+            <p>YOUR FEET DERSERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES,YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITHOUR SHOES.</p>
             <div className="hero-btn">
                 <button>Shop now</button>
-                <button>Category</button>
+                <button className="sec">Category</button>
+            </div>
+
+            <div className="shopping">
+            <p>Also Available On</p>
+
+            <div className="brand-icons">
+                <img src="https://www.citypng.com/public/uploads/preview/hd-flipkart-round-logo-icon-transparent-png-701751694966204grvmunpzzf.png" alt="flipkart" width="50px" />
+                <img src="https://www.citypng.com/public/uploads/preview/official-hq-amazon-a-letter-symbol-logo-icon-70175169479214184uavt0v1m.png" alt="amazon" width="50px" />
+            </div>
             </div>
         
             </div>
-            <div className="Hero-image"></div>
+            <div className="Hero-image">
+                <img src="http://clipart-library.com/images_k/shoe-transparent-background/shoe-transparent-background-12.png" alt="hero" width="500px" />
+            </div>
         </main>
     );
 };
